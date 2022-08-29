e595d91461949db75f69ea87cad477962a993c7e
Fernweh99 <daniele.dimento@gmail.com>
2022-08-29 16:49:45 +0200

    finish ex

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php 
-echo "ciao";
+<?php
+
+//first ex
+$my_text = "supercalifragilisti";
+$text_length = strlen($my_text);
+echo $my_text . "<br/>" . $text_length; 
+
+//second ex
+$word = $_GET["word"];
+echo "<br/>". str_replace($word, "***", $word)."<br/>". strlen(str_replace($word, "***", $word)) ;
